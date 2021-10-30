559dc300fec41749b5df645ca4838caf3f820313
WebSiteKreator <47186073+WebSiteKreator@users.noreply.github.com>
2021-10-30 09:49:07 +0300

    New files

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int a;
+	cout << "Введіть номер цифри: ";
+	cin >> a;
+	switch (a) 
+	{
+		case 0:case 1:cout << "Літери відсутні!"; break;
+		case 2:cout << "A, B, C"; break;
+		case 3:cout << "D, E, F"; break;
+		case 4:cout << "G, H, I"; break;
+		case 5:cout << "J, K, L"; break;
+		case 6:cout << "M, N, O"; break;
+		case 7:cout << "P, Q, R, S"; break;
+		case 8:cout << "T, U, V"; break;
+		case 9:cout << "W, X, Y, Z"; break;
+		default:cout << "Не існує!"; break;
+	}
+}
+
---
+++ ConsoleApplication5.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <math.h>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    double x, Y;
+    cout << "Введіть Х: ";
+    cin >> x;
+    Y = 0;
+    for (int i = 1; i <= 3; i++) {
+        Y += pow(x, i);
+    }
+    Y = log(pow(x, 3) + cos(Y));
+    printf("Результат: %lf", Y);
+}
+
+
